c71a8996901a662b13c567809893c9dc7893f49d
giorgio laera <laeragiorg@gmail.com>
2026-05-13 19:22:29 +0200

    add Auth::user()

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
 // create public function for page
     public function index(){
-        return "questa e la pagina index";
+        $user=Auth::user();
+        return $user->id;
     }
      public function profile(){
         return "questa e la pagina profile";
