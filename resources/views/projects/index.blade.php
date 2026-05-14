0426985e7b94f96111abeeac35ab22145c710555
giorgio laera <laeragiorg@gmail.com>
2026-05-14 23:15:58 +0200

    add index content

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,5 +3,26 @@
 @section('title','i miei post')
 
 @section('content')
-
-@endSection
+<table>
+    <thead>
+        <tr>
+            <th>Titolo</th>
+            <th>Cliente</th>
+            <th>Inizio progetto</th>
+            <th>Fine progetto</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($projects as $project)
+            <tr>
+                <th>{{$project->name}}</th>
+                <th>{{$project->client}}</th>
+                <th>{{$project->start_date}}</th>
+                <th>{{$project->end_date}}</th>
+                <th><a href="">visualizza</a></th>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
