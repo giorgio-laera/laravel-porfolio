15827deae572f4dc3cd203545373bbae52758bf6
giorgio laera <laeragiorg@gmail.com>
2026-05-15 18:44:18 +0200

    update method show

--- app/Http/Controllers/admin/ProjectController.php
+++ app/Http/Controllers/admin/ProjectController.php
@@ -39,7 +39,7 @@ class ProjectController extends Controller
      */
     public function show($id)
     {
-        $project=Project::first('id', $id);
+        $project=Project::where('id', $id)->first();
         // dd($project);
         return view('projects.show',  compact('project'));
     }
