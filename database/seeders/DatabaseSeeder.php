0b92507370e363f2059e7609f194b967d260c1c7
giorgio laera <laeragiorg@gmail.com>
2026-05-25 21:09:04 +0200

    updated call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ProjectTableSeeder::class);
+        $this->call(
+            TypeTableSeeder::class,
+            ProjectTableSeeder::class);
     }
 }
